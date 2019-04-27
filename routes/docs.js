3c0aed02f5ffd74d2f8dcde9c5f5adb20485336b
Nick Andreev <bakasaru@list.ru>
2019-04-28 00:02:58 +0300

    Add single doc getting and change URLs

--- routes/docs.js
+++ routes/docs.js
@@ -10,22 +10,32 @@ mongoose.connect(
 ).catch(error => console.error(error));
 
 /* GET docs listing. */
-router.get('/list', function(req, res) {
+router.get('/', function(req, res) {
     Doc.find({}).sort({updatedAt: 'descending'}).exec((err, docs) => {
-        if (err) return res.status(404).send('Error while getting documents!');
+        if (err)
+            return res.status(404).send('Error while getting documents!');
         return res.send({docs});
     });
 });
 
+router.get('/:id', function(req, res) {
+    Doc.findById(req.params.id, (err, doc) => {
+        if (err)
+            return res.status(404).send({message: err.message});
+        return res.send({ doc });
+    })
+});
+
 /* POST new doc. */
-router.post('/create', function(req, res) {
+router.post('/', function(req, res) {
     let doc = new Doc({
         body: req.body.body,
         title: req.body.title
     });
 
     doc.save(err => {
-        if (err) return res.status(404).send({message: err.message});
+        if (err)
+            return res.status(404).send({message: err.message});
         return res.send({ doc });
     })
 });
