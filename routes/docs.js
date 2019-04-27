6fcae591b28d98229ce2ee36e29e4849505cbd61
Nick Andreev <bakasaru@list.ru>
2019-04-27 23:13:08 +0300

    Add Mongoose connection

--- routes/docs.js
+++ routes/docs.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect(
+    'mongodb://localhost:27017/docs',
+    { useNewUrlParser: true, useCreateIndex: true, }
+).catch(error => console.error(error));
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
 
 module.exports = router;
